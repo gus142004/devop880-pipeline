d5cc6fbbd798363bbe70fe0959e11f1f56c3b352
chayanan suporn <chayanan.sup@ku.th>
2026-03-28 15:23:35 +0000

    Update logo and text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="KU_SubLogo.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          CI/CE Pipeline for React Web app 
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+          Written by Mr.Chayanan Suporn
         </p>
         <p>
           <a
